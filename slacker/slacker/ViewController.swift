f1b28458c62712b3971412f2a1598168ccf5f2ff
Kevin Tran <ktran@tack.tech>
2018-02-28 12:25:03 -0800

    start xcode workspace

---
+++ slacker/slacker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slacker
+//
+//  Created by Kevin Tran on 2/28/18.
+//  Copyright © 2018 ateamhasnoname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
