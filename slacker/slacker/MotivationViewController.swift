148e0e4fc07168da5805531957d153e64fd6983e
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2018-03-08 19:40:19 -0800

    Comments and cleaned up
    Added comments and cleaned up the code a bit

--- slacker/slacker/MotivationViewController.swift
+++ slacker/slacker/MotivationViewController.swift
@@ -9,29 +9,33 @@
 import UIKit
 
 class MotivationViewController: UIViewController {
-
+    
+    //array containing the video identifier for each video
     var videos: [String] = ["ZXsQAXx_ao0", "LPD0z6K0VQY", "-sUKoKQlEC4", "", "EyhOmBPtGNM", "VZ2HcRl4wSk" ]
     
-    //closes the popup
+    //closes the popup and stops the video
     @IBAction func closePopUp(_ sender: Any) {
         self.view.removeFromSuperview()
     }
     
+    //the webview which plays the video
     @IBOutlet var myWebView: UIWebView!
     
     func getVideo(videoCode:String)
     {
+            //alows the video to play without entering the video player
             myWebView.allowsInlineMediaPlayback = true
         
+            //loads the video in the popup
             myWebView.loadHTMLString("<iframe width=\"\(myWebView.frame.width)\" height=\"\(myWebView.frame.height)\" src=\"https://www.youtube.com/embed/\(videoCode)?&playsinline=1\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>", baseURL: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.gray.withAlphaComponent(0.7)
+        self.view.backgroundColor = UIColor.gray.withAlphaComponent(0.7) //dims the background
         
-        getVideo(videoCode: videos[Int(arc4random_uniform(6))]) //loads random video from videos array
+        getVideo(videoCode: videos[Int(arc4random_uniform(6))]) //picks random video from the video array
         // Do any additional setup after loading the view.
     }
 
@@ -40,15 +44,4 @@ class MotivationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
