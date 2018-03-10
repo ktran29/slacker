69face0c0576d107958208e0208b399e2a7a320b
Kevin Tran <ktran@tack.tech>
2018-03-09 19:48:25 -0800

    add lap logging without automatic scrolling

--- slacker/slacker/StopwatchViewController.swift
+++ slacker/slacker/StopwatchViewController.swift
@@ -16,20 +16,20 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
     var time: Double = 0
     var elapsed: Double = 0
     var status: Bool = false
-    var lapTimes: [String] = []
+    var lapTimes: [(String,String)] = []
     var lapElapsed: Double = 0
     var lapTime: Double = 0
     var lapStartTime: Double = 0
 
-
-    @IBOutlet weak var minuteLabel: UILabel!
-    @IBOutlet weak var secondLabel: UILabel!
-    @IBOutlet weak var millisecondLabel: UILabel!
     @IBOutlet weak var stopResumeBtn: UIButton!
     @IBOutlet weak var lapResetBtn: UIButton!
     @IBOutlet weak var startBtn: UIButton!
-    @IBOutlet weak var lapTimerView: UIStackView!
     
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var millisecondLabel: UILabel!
+    
+    @IBOutlet weak var lapTimerView: UIStackView!
     @IBOutlet weak var lapMinuteLabel: UILabel!
     @IBOutlet weak var lapSecondLabel: UILabel!
     @IBOutlet weak var lapMillisecondLabel: UILabel!
@@ -37,21 +37,22 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
     
     @IBOutlet weak var tableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        
-        startBtn.isHidden = false
-        stopResumeBtn.isHidden = true
-        lapResetBtn.isHidden = true
-        lapTimerView.isHidden = true
-
+        self.tableView.rowHeight = 68
+        self.tableView.separatorStyle = .none
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tableView.reloadData()
+        
+        // automatically scrolls to the newest lap time
+        if lapTimes.count > 0 {
+            let indexPath = NSIndexPath(row: lapTimes.count - 1, section: 0)
+            self.tableView.scrollToRow(at: indexPath as IndexPath, at: UITableViewScrollPosition.bottom, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,9 +61,23 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     @IBAction func startStopWatch(_ sender: UIButton) {
-        startBtn.isHidden = true
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.startBtn.alpha = 0
+        }, completion:  {
+            (value: Bool) in
+            self.startBtn.isHidden = true
+        })
+        
         stopResumeBtn.isHidden = false
         lapResetBtn.isHidden = false
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.stopResumeBtn.alpha = 1
+        }, completion:  nil)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.lapResetBtn.alpha = 1
+        }, completion:  nil)
         start()
     }
     
@@ -104,16 +119,18 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func lap() {
+        let st = getTime(time, startTime)
+        let lt = getTime(lapTime, lapStartTime)
         lapTimerView.isHidden = false
         lapStartTime = Date().timeIntervalSinceReferenceDate - lapElapsed
         lapTimer?.invalidate()
         lapTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateLapCounter), userInfo: nil, repeats: true)
         lapElapsed = 0
-        let t = getTime(lapTime, lapStartTime)
-        lapTimes.append("\(t.0):\(t.1):\(t.2)")
+        lapTimes.append(("\(st.0):\(st.1):\(st.2)", "\(lt.0):\(lt.1):\(lt.2)"))
         self.viewWillAppear(false)
     }
     
+    // resets the stopwatch
     func reset() {
         timer?.invalidate()
         lapTimer?.invalidate()
@@ -125,35 +142,43 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
         lapElapsed = 0
         lapTimes = []
         
-        let strReset = String("00")
-        minuteLabel.text = strReset
-        secondLabel.text = strReset
-        millisecondLabel.text = strReset
+        minuteLabel.text = "00"
+        secondLabel.text = "00"
+        millisecondLabel.text = "00"
         
+        startBtn.isHidden = false
+        stopResumeBtn.isHidden = true
+        lapResetBtn.isHidden = true
+        lapTimerView.isHidden = true
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.startBtn.alpha = 1
+        }, completion:  nil)
+        
+        self.stopResumeBtn.setTitle("STOP", for: .normal)
+        self.lapResetBtn.setTitle("LAP", for: .normal)
+        
+        self.tableView.reloadData()
         self.viewDidLoad()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(section)
-        print(lapTimes.count)
         return lapTimes.count
     }
     
+    // updates the lap log with current lap, running time, and lap time
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("here2")
         let cell = tableView.dequeueReusableCell(withIdentifier: "stopwatchCell", for: indexPath) as! StopwatchTableViewCell
         let lapTime = lapTimes[indexPath.row]
         
-        cell.lapNumber.text = "\(indexPath.row)"
-        cell.lapTime.text = "\(lapTime)"
-        
-        let t = getTime(time, startTime)
-        
-        cell.overallTime.text = "\(t.0):\(t.1):\(t.2)"
+        cell.lapNumber.text = "\(indexPath.row + 1)"
+        cell.overallTime.text = "\(lapTime.0)"
+        cell.lapTime.text = "\(lapTime.1)"
         
         return cell
     }
 
+    // updates the main time
     @objc func updateCounter() {
         let t = getTime(time, startTime)
 
@@ -163,6 +188,7 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
 
     }
     
+    // updates the lap time
     @objc func updateLapCounter() {
         let t = getTime(lapTime, lapStartTime)
         
@@ -172,6 +198,7 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
         
     }
     
+    // converts double to a readable time
     func getTime(_ givenTime: Double, _ givenStartTime: Double) -> (String, String, String) {
         var givenTime = Date().timeIntervalSinceReferenceDate - givenStartTime
         
@@ -183,7 +210,7 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
         
         let milliseconds = UInt8(givenTime * 100)
         
-        let strMinutes = String(format: "%02d", minutes)
+        let strMinutes = minutes < UInt8(10.0) ? String(format: "%02d", minutes) : "\(minutes)"
         let strSeconds = String(format: "%02d", seconds)
         let strMilliseconds = String(format: "%02d", milliseconds)
         
